959e9692e3bb4fe4514a2e897b48fc1ff0b358d6
John <johnzengshi@gmail.com>
2025-02-27 22:20:42 +0800

    style(css, routes): 更新颜色主题并调整根路由布局 将颜色主题改为深色，并在根路由中添加上下安全区。

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,11 +1,13 @@
 import { createRootRoute, Outlet } from "@tanstack/react-router";
-import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+import { SafeArea } from "antd-mobile";
 
 export const Route = createRootRoute({
   component: () => (
     <div className="bg-[#141414] w-[100vw] min-h-[100vh]">
+      <SafeArea position="top" />
       <Outlet />
       {/* <TanStackRouterDevtools /> */}
+      <SafeArea position="bottom" />
     </div>
   ),
 });
